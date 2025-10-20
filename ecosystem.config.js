5a68b2a577cb87d10f428c6a5786e94ff2c5b720
Ross Phillips <rossphillips@Rosss-Mac-mini.local>
2025-10-20 15:31:46 +0100

    Fix PM2 config for ES modules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   apps: [{
     name: 'pi-smart-display',
     script: 'server/server.js',
-    cwd: '/home/pi/pi-smart-display',
+    cwd: '/home/ross/pi-smart-display',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
@@ -10,9 +10,9 @@ module.exports = {
     restart_delay: 1000,
     max_restarts: 10,
     min_uptime: '10s',
-    error_file: '/home/pi/pi-smart-display/logs/err.log',
-    out_file: '/home/pi/pi-smart-display/logs/out.log',
-    log_file: '/home/pi/pi-smart-display/logs/combined.log',
+    error_file: '/home/ross/pi-smart-display/logs/err.log',
+    out_file: '/home/ross/pi-smart-display/logs/out.log',
+    log_file: '/home/ross/pi-smart-display/logs/combined.log',
     time: true
   }]
 };
